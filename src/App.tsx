f0776e4937d493c59c395442b96aca8bb9bcb425
Byungjin-Kim <composerjins@gmail.com>
2024-03-29 23:45:10 -0500

    interface, useState, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,26 @@ import axios from 'axios';
 
 import './App.css';
 
+import Nasaphoto from './Components/Nasaphoto';
+
+interface NASAData {
+  // Define NASA API data type
+}
+
 function App() {
+  const [data, setData] = useState<NASAData | null>(null);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_NASA_API_KEY;
+    axios.get<NASAData>(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch(err => console.error(err));
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
