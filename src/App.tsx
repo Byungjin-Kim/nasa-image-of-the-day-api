04660714538f506944218433477432c8ad1147c5
Byungjin-Kim <composerjins@gmail.com>
2024-04-01 23:40:21 -0500

    Conditional rendering using the &&

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,47 @@ import Nasaphoto from './Components/Nasaphoto';
 
 interface NASAData {
   // Define NASA API data type
+  title: string;
+
 }
+
+const apiKey = process.env.REACT_APP_NASA_API_KEY;
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+
 
 function App() {
   const [data, setData] = useState<NASAData | null>(null);
 
   useEffect(() => {
-    const apiKey = process.env.REACT_APP_NASA_API_KEY;
-    axios.get<NASAData>(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
-      .then(res => {
-        console.log(res.data);
-        setData(res.data);
-      })
-      .catch(err => console.error(err));
+    function fetchPhoto() {
+      axios.get(URL)
+        .then(res => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch(err => {
+          console.log(err.message);
+        })
+    }
+    fetchPhoto();
   }, [])
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World!</h1>
+        </header>
+      </div>
+      <div className='Nasa-photo-wrapper'>
+        {/*Conditional rendering using the &&*/}
+        {data && <h2>{data.title}</h2>}
+
+      </div>
+
+    </>
+
   );
 }
 
